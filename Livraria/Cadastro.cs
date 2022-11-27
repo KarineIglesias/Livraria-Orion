38f29a585c2f3de1983bb6bff62807cd02c20c68
KarineIglesias <100173562+KarineIglesias@users.noreply.github.com>
2022-11-26 22:11:19 -0300

    Adicionar arquivos de projeto.

---
+++ Livraria/Cadastro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.collections.generic;
+using System.linq;
+using System.Text;
+using System.threading.tasks;
+
+namespace Cadastro.Classes
+{ 
+
+
+     public class Televendas
+    {
+
+    public string NomeLoja { get; set; }
+	public int Quantidade { get; set; }
+	public string Categoria { get; set}
+	
+	}
+  
+}
+  
+
---
+++ Livraria/Program.cs
@@ -0,0 +1,16 @@
+﻿using System
+using  Cadastro.classes;
+ 
+{
+     public static void Main(string[]args)
+    { 
+      var Cadastro = new Cadastro();
+        Cadastro.NomeLoja ="Orion"
+        Cadastro.Quantidade = 1;
+        Cadastro.Categoria ="Suspense"
+    }
+
+    Console.WriteLine("Atendimento realizado com sucesso");
+    Console.ReadLine();
+    }
+}
